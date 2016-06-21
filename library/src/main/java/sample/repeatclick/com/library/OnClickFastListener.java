21a4a8a790b6fd3e593c827b2668ee65417b7dbc
yipianfengye <1161944036@qq.com>
2016-06-21 09:49:29 +0800

    更新快速点击事件,可配置快速点击事件的时间间隔

--- library/src/main/java/sample/repeatclick/com/library/OnClickFastListener.java
+++ library/src/main/java/sample/repeatclick/com/library/OnClickFastListener.java
@@ -9,10 +9,10 @@ import android.view.View;
 public abstract class OnClickFastListener extends BaseClickListener {
 
     // 防止快速点击默认等待时长为900ms
-    private static long DELAY_TIME = 900;
-    private static long lastClickTime;
+    private long DELAY_TIME = 900;
+    private long lastClickTime;
 
-    private static boolean isFastDoubleClick() {
+    private boolean isFastDoubleClick() {
         long time = System.currentTimeMillis();
         long timeD = time - lastClickTime;
         if (0 < timeD && timeD < DELAY_TIME) {
@@ -25,12 +25,28 @@ public abstract class OnClickFastListener extends BaseClickListener {
     @Override
     public void onClick(View v) {
         // 判断当前点击事件与前一次点击事件时间间隔是否小于阙值
-        if (OnClickFastListener.isFastDoubleClick()) {
+        if (isFastDoubleClick()) {
             return;
         }
 
         onFastClick(v);
     }
 
+    /**
+     * 设置默认快速点击事件时间间隔
+     * @param delay_time
+     * @return
+     */
+    public OnClickFastListener setLastClickTime(long delay_time) {
+
+        this.DELAY_TIME = delay_time;
+
+        return this;
+    }
+
+    /**
+     * 快速点击事件回调方法
+     * @param v
+     */
     public abstract void onFastClick(View v);
 }
